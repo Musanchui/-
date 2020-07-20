7da916f4465c2efb2b1a61618480ed992ca632d1
musanchui <1041982876@qq.com>
2020-07-20 23:09:06 +0800

    完成了粗略的用户界面，尚未实现输入功能

---
+++ Account.java
@@ -0,0 +1,18 @@
+class Account{
+	public static void main(String [] args){
+		int balance = 10000;
+		String details ="收支\t账户金额\t收支金额\t说  明";
+		System.out.println("----------家庭收支账本----------");
+		System.out.println("");
+		System.out.println("1收支明细");
+		System.out.println("2登记收入");
+		System.out.println("3登记支出");
+		System.out.println("4退出");
+		System.out.println("");
+		System.out.println("请选择1-4：");
+		Utility utility= new Utility();
+		
+		
+		
+	}
+}
