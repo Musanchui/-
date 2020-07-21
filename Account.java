cc011d63b65066c8a5050df88a24473d7e254959
musanchui <1041982876@qq.com>
2020-07-21 20:09:15 +0800

    实现了输入收支与输出有限的两行收支。无法存储数据

--- Account.java
+++ Account.java
@@ -1,18 +1,54 @@
 class Account{
 	public static void main(String [] args){
+		boolean conti = true;
 		int balance = 10000;
-		String details ="收支\t账户金额\t收支金额\t说  明";
-		System.out.println("----------家庭收支账本----------");
-		System.out.println("");
-		System.out.println("1收支明细");
-		System.out.println("2登记收入");
-		System.out.println("3登记支出");
-		System.out.println("4退出");
-		System.out.println("");
-		System.out.println("请选择1-4：");
-		Utility utility= new Utility();
-		
-		
-		
+		Utility utility = new Utility();
+		int in=0;
+		int out=0;
+		String inExplain = "";
+		String outExplain ="";
+		while(conti){
+			System.out.println("----------Family Account Book----------");
+			System.out.println("");
+			System.out.println("1Income and expenditure details");
+			System.out.println("2Registe income");
+			System.out.println("3Registe expenditure");
+			System.out.println("4Quit");
+			System.out.println("");
+			System.out.print("select in 1-4:");
+			
+			char userInput = utility.readMenuSelection();
+			String users = String.valueOf(userInput);
+			if(users.equals("1")){
+				System.out.println("\tbalance\tturnover\texplain");
+				System.out.println("income\t"+balance+"\t"+in+"\t"+inExplain);
+				System.out.println("output\t"+balance+"\t"+out+"\t"+outExplain);
+				//break;
+			}
+			if(users.equals("2")){
+				System.out.println("income number:");
+				int inNum = utility.readNumber();
+				in = in+inNum;
+				balance = balance+in;
+				System.out.println("income explaination:");
+				inExplain=utility.readString();
+				//break;
+			}
+			if(users.equals("3")){
+				System.out.println("output number:");
+				int outNum = utility.readNumber();
+				out = out+outNum;
+				balance = balance-out;
+				System.out.println("outcome explaination:");
+				outExplain=utility.readString();
+				//break;
+			}
+			if(users.equals("4")){
+				System.out.println("Are you sure to quit? Y/N");
+				char anwser = utility.readConfirmSelection();
+				String a = String.valueOf(anwser);
+				if(a.equals("Y"))conti = false;
+			}
+		}		
 	}
 }
